1049d80fcc149f655c75d199f49b581ca1e7d268
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-11-24 19:28:45 +0300

    temp

--- __tests__/taskManager.test.ts
+++ __tests__/taskManager.test.ts
@@ -2,23 +2,23 @@ import { Task, TaskRepository, TaskState } from '../src/models';
 import TaskManager from '../src/taskManager';
 import Repository from '../src/repository'
 
-const tasks = [ 
-    {
-        id: 1,
-        name: 'task1',
-        state: TaskState.active,
-    }, 
-    {
-        id: 2,
-        name: 'task2',
-        state: TaskState.done,
-    }
-];
-
 test('get all tasks', () => {
+    const tasks = [ 
+        {
+            id: 1,
+            name: 'task1',
+            state: TaskState.active,
+        }, 
+        {
+            id: 2,
+            name: 'task2',
+            state: TaskState.done,
+        }
+    ];
+
     const testRepository: TaskRepository = {
         getAll: jest.fn(() => tasks)
-    }
+    };
 
     const manager = new TaskManager(testRepository);
     const allTasks = manager.getAllTasks();
@@ -28,12 +28,45 @@ test('get all tasks', () => {
 });
 
 test('get active tasks', () => {
+    const tasks = [ 
+        {
+            id: 1,
+            name: 'task1',
+            state: TaskState.active,
+        }, 
+        {
+            id: 2,
+            name: 'task2',
+            state: TaskState.active,
+        }, 
+        {
+            id: 3,
+            name: 'task3',
+            state: TaskState.done,
+        }
+    ];
+
     const testRepository: TaskRepository = {
         getAll: jest.fn(() => tasks)
-    }
+    };
 
     const manager = new TaskManager(testRepository);
     const activeTasks = manager.getActiveTasks();
+
+    const doneTasks = [
+        {
+            id: 1,
+            name: 'task1',
+            state: TaskState.done,
+        }
+    ];
+
+    const testRepositoryDoneTasks: TaskRepository = {
+        getAll: jest.fn(() => doneTasks)
+    };
+
+    const managerDoneTasks = new TaskManager(testRepositoryDoneTasks);
+    const emptyActiveTasks = managerDoneTasks.getActiveTasks();
 
     expect(activeTasks).toEqual([
         {
@@ -41,28 +74,50 @@ test('get active tasks', () => {
             name: 'task1',
             state: TaskState.active,
         }, 
+        {
+            id: 2,
+            name: 'task2',
+            state: TaskState.active,
+        } 
     ]);
+    expect(emptyActiveTasks).toEqual([]);
 });
 
 test.skip('add task', () => {
+    const tasks = [ 
+        {
+            id: 1,
+            name: 'task1',
+            state: TaskState.active,
+        }, 
+        {
+            id: 2,
+            name: 'task2',
+            state: TaskState.done,
+        }
+    ];
+
     const newTask: Task = {
         id: 3,
         name: 'task3',
         state: TaskState.active,
     };
 
-    const testRepository = new Repository(tasks);
+    const testRepository: TaskRepository = {
+        addItem: jest.fn(() => tasks)
+    };
+
     const manager = new TaskManager(testRepository);
-    manager.addTask(newTask);
+    const activeTasks = manager.getActiveTasks();
 
     // expect(TaskRepositoryMock.prototype.addItem).toHaveBeenCalledTimes(1);
 });
 
 test.skip('delete task', () => {
-    const testRepository = new Repository(tasks);
-    const manager = new TaskManager(testRepository);
+    // const testRepository = new Repository(tasks);
+    // const manager = new TaskManager(testRepository);
     const deletedId = 2;
-    manager.deleteTask(deletedId);
+    // manager.deleteTask(deletedId);
 
     // expect(TaskRepositoryMock.prototype.deleteItem).toHaveBeenCalledTimes(1);
 });
@@ -73,9 +128,9 @@ test.skip('update task', () => {
         name: 'newName',
     };
 
-    const testRepository = new Repository(tasks);
-    const manager = new TaskManager(testRepository);
-    manager.updateTask(updatedId, newData);
+    // const testRepository = new Repository(tasks);
+    // const manager = new TaskManager(testRepository);
+    // manager.updateTask(updatedId, newData);
 
     // expect(TaskRepositoryMock.prototype.updateItem).toHaveBeenCalledTimes(1);
 });
